4b13c6de5c2a28138e0e9bbfbaa5c13a34eac7db
audrey06lee05 <audrey06lee05@gmail.com>
2024-09-04 23:27:00 +0100

    added icons and links button in footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./footer.css";
+import { BsInstagram, BsFacebook } from "react-icons/bs";
 
 const Footer = () => {
   return (
@@ -11,6 +12,20 @@ const Footer = () => {
         <h3>
           <a href="/contact">Contact</a>
         </h3>
+        <a
+          className="icon-button"
+          href="https://www.instagram.com/my_cup_of_hk_tea/"
+          target="_blank"
+        >
+          <BsInstagram />
+        </a>
+        <a
+          className="icon-button"
+          href="https://www.facebook.com/share/2rE1zBPjqbKfWg6p/?mibextid=LQQJ4d"
+          target="_blank"
+        >
+          <BsFacebook />
+        </a>
       </div>
       <p id="copyright">© All Rights Reserved</p>
     </footer>
