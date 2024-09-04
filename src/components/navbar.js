3f344064b57ed2566c8498db6e686d1a4771cac1
audrey06lee05 <audrey06lee05@gmail.com>
2024-09-04 22:27:53 +0100

    added logo.png and navbar.css

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,13 @@
 import React from "react";
+import logo from "../img/logo.png";
+import "./navbar.css";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
+      <img src={logo} alt="Logo" />
+      <h1>My Cup of HK Tea</h1>
+
       <a href="/" className="nav-link">
         Home
       </a>
