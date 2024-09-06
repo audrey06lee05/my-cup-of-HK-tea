7b43458cc29ca394c8b5c6dc50614b0925a2b1dd
audrey06lee05 <audrey06lee05@gmail.com>
2024-09-06 18:20:50 +0100

    added a new address in contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,9 +11,9 @@ const ContactPage = () => {
         <figure>
           <img src={leedsstore} alt="Leeds Store"></img>
         </figure>
-        <article className="leeds-store">
+        <article className="stores">
           <h1>Contact Us</h1>
-          <p>Address:</p>
+          <p>Leeds Address:</p>
           <a
             className="address"
             href="https://maps.app.goo.gl/2C5KVvqnBfPgumVC6"
@@ -21,7 +21,7 @@ const ContactPage = () => {
           >
             <p>No.16, Krikgate Market, Leeds LS2 7HY</p>
           </a>
-          <p>Contact us on social media!</p>
+
           <div className="icon-buttons">
             <a
               href="https://www.instagram.com/my_cup_of_hk_tea/"
@@ -38,6 +38,32 @@ const ContactPage = () => {
               <BsFacebook />
             </a>
           </div>
+          <p>Reading Address:</p>
+          <a
+            className="address"
+            href="https://maps.app.goo.gl/dEWZepBnGFRCDKQW7"
+            target="_blank"
+          >
+            <p>77-79 kings road, Reading, England Rg1 3dd</p>
+          </a>
+
+          <div className="icon-buttons">
+            <a
+              href="https://www.instagram.com/mcohktr/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsInstagram />
+            </a>
+            <a
+              href="https://www.facebook.com/share/2rE1zBPjqbKfWg6p/?mibextid=LQQJ4d"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsFacebook />
+            </a>
+          </div>
+          <p>Contact us on social media!</p>
         </article>
       </main>
     </>
